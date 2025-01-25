0c11fa721dd7bcf60f601ac382cee64a5c061ba4
NIKHIL MANAM <manamnikhil86@gmail.com>
2025-01-25 13:40:19 +0530

    added download exxcel

--- frontend/src/components/Admin/adminpage.jsx
+++ frontend/src/components/Admin/adminpage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './admin.css';
+import './admin.css'; 
 import { useNavigate } from 'react-router-dom';
 import { adminLogin } from '../../auth/Adminauth';
 
