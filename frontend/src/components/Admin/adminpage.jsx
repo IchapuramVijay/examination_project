b36d332a6609a5ce953ffc0e4eaeeb6822b26bbe
NIKHIL MANAM <manamnikhil86@gmail.com>
2025-02-18 13:20:09 +0530

    Noerrors

--- frontend/src/components/Admin/adminpage.jsx
+++ frontend/src/components/Admin/adminpage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import './Admin.css';
+import './admin.css';
 
 import { useNavigate } from 'react-router-dom';
 import { adminLogin } from '../../auth/Adminauth';
